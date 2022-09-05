f68e50992d1800361ad8733723e8cf6d62f6c94b
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-09-06 00:58:29 +0530

    symbols added, format modified

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
      	/*Icon*/    /*Command*/    /*Update Interval*/    /*Update Signal*/
-	{" Vol: ",    "amixer -D pulse get Master | tail -n 1 | awk '{print $5}' | awk '{print substr($0, 2, length($0) - 3)}'",    1,    0}, 
+	{" 💻 ",    "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",    10,    0},
 
-    	{"Brg: ",    "~/Projects/dwmblocks/brightnessstatus.o",    1,    0},
+	{"* ",    "~/Projects/dwmblocks/brightnessstatus.o",    1,    0},
+	
+	{"🔊 ",  "amixer -D pulse get Master | tail -n 1 | awk '{print $5}' | awk '{print substr($0, 2, length($0) - 3)}'",    1,    0},
 
-	{"Mem: ",    "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",    10,    0},
-
-	{"Bat: ",    "~/Projects/dwmblocks/batterystatus.o",    10,    0},
+	{"",    "~/Projects/dwmblocks/batterystatus.o",    10,    0},
 
 	{"",    "date '+%a, %b %d | %I:%M%p '",    10,    0},
 };
