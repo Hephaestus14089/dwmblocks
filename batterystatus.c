0fb235ad2cf401b2ffa9d5d245a73e54f70e4f5a
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-09-06 00:49:32 +0530

    battery symbol added
    conditional change

--- batterystatus.c
+++ batterystatus.c
@@ -7,25 +7,41 @@
 #include <errno.h>
 #include <stdarg.h>
 
-struct bat {
-
+typedef struct bat {
 	int charge;
-	char status;
-};
+	char* symbol;
+} bat;
 
-typedef struct bat bat;
+char*
+getbatsymbol(int charge, int isCharging) {
+    char* symbol = NULL;
 
-bat *
-getbattery(){
+    if (isCharging == 1)
+        symbol = "";
+    else if (charge >= 85)
+        symbol = "";
+    else if (charge >= 25)
+        symbol = "";
+    else if (charge >= 10)
+        symbol = "";
+    else
+        symbol = "";
+
+    return symbol;
+}
+
+bat*
+getbattery() {
     
     int curr_charge = 0;
     char *status = malloc(sizeof(char)*12);
-    char s = '?';    
+    // char s = '?';
     FILE *fp = NULL;
+    int isCharging = 0;
     
     if ((fp = fopen(BATT_NOW, "r"))) {
         
-	fscanf(fp, "%d\n", &curr_charge);
+        fscanf(fp, "%d\n", &curr_charge);
         fclose(fp);
         
         fp = fopen(BATT_STATUS, "r");
@@ -33,27 +49,21 @@ getbattery(){
         fclose(fp);
 
         if (strcmp(status,"Charging") == 0)
-            s = '+';
-        if (strcmp(status,"Discharging") == 0)
-            s = '-';
-        if (strcmp(status,"Full") == 0)
-            s = '=';
+            isCharging = 1;
     }
 
     bat *curr_bat = (bat*)malloc(sizeof(bat));
 
     curr_bat->charge = curr_charge;
-    curr_bat->status = s;
+    curr_bat->symbol = getbatsymbol(curr_bat->charge, isCharging);
 
     return curr_bat;
 }
 
 
 int main() {
-
 	bat *curr_bat = getbattery();
-	
-	printf("%d(%c)", curr_bat->charge, curr_bat->status);
+	printf("%s %d", curr_bat->symbol, curr_bat->charge);
 	
 	return 0;
 }
